d2e56d8f1655304491266a33ea55470b9e1b6bbf
TomasBrigido <tomas.brigido@mi.unc.edu.ar>
2026-03-15 17:15:32 -0300

    Add CMakeList.txt

---
+++ build/CMakeFiles/programa.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/programa.dir/src/main.c.o"
+  "CMakeFiles/programa.dir/src/main.c.o.d"
+  "CMakeFiles/programa.dir/src/suma.c.o"
+  "CMakeFiles/programa.dir/src/suma.c.o.d"
+  "programa"
+  "programa.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/programa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/programa.dir/src/suma.c.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/programa.dir/src/suma.c.o: \
+ /home/tomas/Documentos/prueba/prueba/src/suma.c \
+ /usr/include/stdc-predef.h \
+ /home/tomas/Documentos/prueba/prueba/include/suma.h
